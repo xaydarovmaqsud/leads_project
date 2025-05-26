711d83380a4105e254e1aa599b1942d446e4807f
Xaydarov Maqsud <103387007+xaydarovmaqsud@users.noreply.github.com>
2025-05-26 10:38:55 +0500

    Update settings.py

--- leads_project/settings.py
+++ leads_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&9-8#!vj1leorlh%0hd8wwlkx!fq6#z)iup^g$l+@i7a(x4gm6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
