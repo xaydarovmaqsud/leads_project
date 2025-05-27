e8f6f09857a389692591310173a35d74f56572b3
xaydarovmaqsud <xaydarovmaqsud2002@gmail.com>
2025-05-27 11:01:35 +0500

    update

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Lead
 
-# Register your models here.
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email')
+
+
+from rest_framework.authtoken.models import Token
+
+admin.site.register(Token)
